0eff2248b87bb2b3f20b3f0bc428ed6fea4e9c7b
lijiali <lijiali@zhihu.com>
2020-09-03 21:17:29 +0800

    feat: 新增聊天用户列表

--- src/pages/talkUserList/index.js
+++ src/pages/talkUserList/index.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import "./index.less";
 
+import mockData from './mockData'
+
 export const TalkUserListPage = () => {
   return <div className="talkUserList">talkUserList</div>;
 };
