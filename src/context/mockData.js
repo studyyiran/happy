b7ae09d64742aaa3d72bab42b01cff0d11010025
yiranzi <570932296@qq.com>
2020-09-04 16:34:28 +0800

    feat: commit

--- src/context/mockData.js
+++ src/context/mockData.js
@@ -33,7 +33,7 @@ export const userInfoArr = [
     level: 90, // 知聊的打分 表示知聊对这个人的聊天能力认可度
     // zhitalkTag: ["数据帝", "百科全书"], // 用户聊完后对于一个人社交倾向，知聊用户对他的聊天
     name: "猴子",
-    avatar: "https://pic2.zhimg.com/v2-09797f959fa8f75c2295d38c49cb51ab_xll.jpg", // 头像我也想随机 额啊啊啊啊
+    avatar: "https://pic3.zhimg.com/v2-12ef91a3f1e91e70bd3480d755e058b1_xl.jpg", // 头像我也想随机 额啊啊啊啊
     sex: "男",
     creationsSum: 1141, // 客观创作型
     likeSum: "9.5万", // 用户量
@@ -42,7 +42,7 @@ export const userInfoArr = [
   {
     realUrl: "https://www.zhihu.com/people/houziliaorenwu", // 黑猫
     infoImg: require("./res/黑猫厨房.jpeg"),
-    intro: "不懂文学的码农不是好的创作者", // 第六组代言者评价
+    intro: "很细心，做饭特别好吃", // 第六组代言者评价
     desc: "职场和数据分析 创作大佬-大V",
     level: 90, // 知聊的打分 表示知聊对这个人的聊天能力认可度
     zhitalkTag: ["热情", "萌", "细致"],
@@ -58,7 +58,7 @@ export const userInfoArr = [
     intro: "历史学家", // 第六组代言者评价
     level: 70, // 知聊的打分 表示知聊对这个人的聊天能力认可度
     name: "红护",
-    avatar: "https://pic1.zhimg.com/v2-7431714568e024f3d05664513176532f.jpg",
+    avatar: "https://pic4.zhimg.com/v2-3f758df6771aeff1f606f451a1d68373_xl.jpg",
     sex: "男",
     creationsSum: 300, // 客观创作型
     likeSum: "2.4万", // 用户量
@@ -69,7 +69,7 @@ export const userInfoArr = [
     intro: "历史学家", // 第六组代言者评价
     level: 40, // 知聊的打分 表示知聊对这个人的聊天能力认可度
     name: "天雨白",
-    avatar: "https://pic1.zhimg.com/v2-7431714568e024f3d05664513176532f.jpg",
+    avatar: "https://pic1.zhimg.com/033204ff4f44d6f9463279ac08278e6b_xl.jpg",
     sex: "女",
     creationsSum: 100, // 客观创作型
     likeSum: "1500", // 用户量
@@ -80,7 +80,7 @@ export const userInfoArr = [
     intro: "历史学家", // 第六组代言者评价
     level: 120, // 知聊的打分 表示知聊对这个人的聊天能力认可度
     name: "温柔JUNZ",
-    avatar: "https://pic1.zhimg.com/v2-7431714568e024f3d05664513176532f.jpg",
+    avatar: "https://pic4.zhimg.com/v2-75aeae453306b053fadd01804f6e5939_xl.jpg",
     sex: "男",
     creationsSum: 400, // 客观创作型
     likeSum: "100", // 用户量
