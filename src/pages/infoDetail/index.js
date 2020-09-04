e809f61c12b8fc14a1ca7f5d6d1500d3368c6b53
yiranzi <570932296@qq.com>
2020-09-04 09:06:32 +0800

    feat: 跳转

--- src/pages/infoDetail/index.js
+++ src/pages/infoDetail/index.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from "react";
+import { message } from "antd";
 
 import { Button } from "antd";
 
@@ -8,15 +9,37 @@ import { StoreZhiTalkContext } from "../../context";
 export const InfoDetailPage = (props) => {
   const storeZhiTalkContext = useContext(StoreZhiTalkContext);
   const { storeZhiTalkContextValue } = storeZhiTalkContext;
-  const { currentFriendInfo } = storeZhiTalkContextValue;
+  const { currentFriendInfo, currentTalkInfo } = storeZhiTalkContextValue;
   const { img } = currentFriendInfo;
+  const [haveInvite, setHaveInvite] = useState(false);
 
   return (
     <div className="InfoDetailPage">
       <img className="info" src={img} />
       <div className="button">
-        <Button type="primary">不感兴趣</Button>
-        <Button type="primary">发起邀请</Button>
+        <Button
+          type="primary"
+          onClick={() => {
+            message.info(`不再看Ta「${currentTalkInfo.title}」建立的创作室了`);
+          }}
+        >
+          不感兴趣
+        </Button>
+        <Button
+          type="primary"
+          onClick={() => {
+            if (haveInvite) {
+              return;
+            }
+            setHaveInvite(true);
+            message.success("已成功发出邀请，10 分钟内将会答复");
+            window.setTimeout(() => {
+              props.history.push("");
+            }, 3000);
+          }}
+        >
+          {haveInvite ? "已经邀请" : "发起邀请"}
+        </Button>
       </div>
     </div>
   );
