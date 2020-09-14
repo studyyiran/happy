ccdb18865ab739746c90c8e0441bd6114e5c20b8
yiranzi <570932296@qq.com>
2020-09-14 16:00:07 +0800

    feat: commit

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ npm run build
 # 将关键的文件扔到build里面
 #mv buildserver/server.js build/server.js
 ~/auto/pm2start.sh
-scp -ri ~/.ssh/github_studyyiran build root@139.224.2.112:/data/www/hackathon
+scp -ri ~/.ssh/github_studyyiran build root@47.56.177.240:/data/frontend/hackathon
